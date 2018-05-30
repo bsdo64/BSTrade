6b079b946bd4b1d2fd5161e6474f562e9c9d281c
ByeongSu Do <bsdo@naver.com>
2018-05-30 15:29:58 +0900

    merge source_clients

--- BSTrade/app.py
+++ BSTrade/app.py
@@ -1,7 +1,7 @@
-from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
 import sys
 from forms import MainWindow
+from source_clients.bitmexwsclient import BitmexWsClient
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- BSTrade/forms/__init__.py
+++ BSTrade/forms/__init__.py
@@ -1,1 +1,6 @@
+__version__ = '0.1'
+__all__ = [
+    'MainWindow'
+]
+
 from .mainwindow import MainWindow
